2c59ca9d881b8879c46d816bc45d2ce032c71b36
Juan David Pastas <juandavid1707@gmail.com>
2015-01-11 23:46:40 -0500

    Update sort_by_applicants.js

--- sort_by_applicants.js
+++ sort_by_applicants.js
@@ -13,6 +13,6 @@ $.get($('.oVisitedLink:first')[0].href, function(r){
 
 
 // one line
-$('.oVisitedLink').map(function(i,e) {$.get(e.href,function(r){ $(e).after($(r).find('#jobActivitySection tr:contains(Applicants) td:last').html().trim())  })})
+$('.oVisitedLink').map(function(i,e) {$.get(e.href,function(r){ a=$(r).filter(':has(#jobActivitySection)').find('#jobActivitySection tr:contains(Applicants) td:last').html(); a= (a||'').trim(); $(e).after(a)  })})
 
-$.get($('.oVisitedLink:first')[0].href, function(r){ y=r;console.log(x=$(r).find('#jobActivitySection tr:contains(Applicants) td:last')) })
+$.get($('.oVisitedLink:first')[0].href, function(r){ y=r;console.log(x=$(r).filter('#jobActivitySection').find('tr:contains(Applicants) td:last').html().trim() })
